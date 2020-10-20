d02a91711ddcfbd3aec34df835327b391ac159ca
Gonzaa25 <gonzalo.gauto@gmail.com>
2020-10-20 13:53:12 -0300

    env port

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const app = express()
 const port = 3000
 var { graphqlHTTP } = require('express-graphql');
 const graphschema = require('./src/schema');
-
+const dotenv = require('dotenv');
+dotenv.config();
 
 
 app.use('/graphql', graphqlHTTP({
@@ -11,4 +12,4 @@ app.use('/graphql', graphqlHTTP({
   graphiql: true,
 }));
 
-app.listen(port, () => console.log(`Example app listening on port port!`))
+app.listen(process.env.PORT, () => console.log(`Example app listening on port ${process.env.PORT}!`))
