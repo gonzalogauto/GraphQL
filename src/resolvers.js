c00a6a31b3bc67660de309485a0bb1f9722d353c
Gonzaa25 <gonzalo.gauto@gmail.com>
2020-10-21 15:04:29 -0300

    resolvers update

--- src/resolvers.js
+++ src/resolvers.js
@@ -1,13 +1,26 @@
 const { comentarios } = require('../data.json');
-
+const axios = require('axios').default;
 module.exports ={
     resolvers:{ 
         Query:{
-            comentarios: () => comentarios,
-            totalcomentarios:()=>comentarios.length,
-            comentarios_buscar:(args)=>{
-                let usuario=args.usuario;
-                return comentarios.filter(element=>element.usuario==usuario);
+            facturasmanuales:async(argument)=>{
+                console.log(argument);
+                try {
+                    var head={
+                        "IdCliente":process.env.ID_CLIENTE,
+                        "Authorization":process.env.API_TOKEN
+                    }
+                    const resp = await axios.get(process.env.HOST_URI+"Factura/?limit=10", {
+                        headers: head
+                    });
+                    //console.log(resp.data);
+                    return resp.data;
+                } catch (err) {
+                    // Handle Error Here
+                    //console.error(err);
+                    return err;
+                }
+                
             }
         }
     }
