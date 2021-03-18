ae7147abbc901bec4ced74b28958da3ea161bfd1
Gonzaa25 <gonzalo.gauto@gmail.com>
2021-03-18 12:40:12 -0300

    resolvers update - can query arts by cod

--- src/resolvers.js
+++ src/resolvers.js
@@ -2,7 +2,7 @@ const axios = require('axios').default;
 module.exports ={
     resolvers:{ 
         Query:{
-            facturasmanuales:async(argument)=>{
+            facturasmanuales:async(obj,argument,context,info)=>{
                 console.log(argument);
                 try {
                     var head={
@@ -21,7 +21,7 @@ module.exports ={
                 }
                 
             },
-            clientes:async(argument)=>{
+            clientes:async(obj,argument,context,info)=>{
                 console.log(argument);
                 try {
                     var head={
@@ -33,6 +33,53 @@ module.exports ={
                     });
                     //console.log(resp.data);
                     return resp.data;
+                } catch (err) {
+                    // Handle Error Here
+                    //console.error(err);
+                    return err;
+                }
+                
+            },
+            stockyprecios:async(obj,argument,context,info)=>{
+                console.log(argument);
+                try {
+                    var head={
+                        "IdCliente":process.env.ID_CLIENTE,
+                        "Authorization":process.env.API_TOKEN
+                    }
+                    const resp = await axios.get(process.env.HOST_URI+"ConsultaStockYPrecios", {
+                        headers: head
+                    });
+                    //console.log(resp.data);
+                    return resp.data;
+                } catch (err) {
+                    // Handle Error Here
+                    //console.error(err);
+                    return err;
+                }
+                
+            },
+            articulos:async(obj,argument,context,info)=>{
+                console.log(argument.codigo);
+                try {
+                    var head={
+                        "IdCliente":process.env.ID_CLIENTE,
+                        "Authorization":process.env.API_TOKEN
+                    }
+                    if (argument.codigo!=null) {
+                        console.log("hay info")
+                        const resp = await axios.get(process.env.HOST_URI+"Articulo?query="+argument.codigo, {
+                            headers: head
+                        });
+                        return resp.data;
+                    } else {
+                        const resp = await axios.get(process.env.HOST_URI+"Articulo", {
+                            headers: head
+                        });
+                        return resp.data;
+                        
+                    }
+                    
                 } catch (err) {
                     // Handle Error Here
                     //console.error(err);
